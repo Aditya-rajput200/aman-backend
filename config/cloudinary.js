8d2838c6e31c467f50cd8c61efd113cbcaeb0116
Aditya <adityarajputsw2020@gmail.com>
2026-03-14 23:36:37 +0530

    done with

--- config/cloudinary.js
+++ config/cloudinary.js
@@ -1,22 +1,24 @@
-const cloudinary = require('cloudinary').v2;
-const { CloudinaryStorage } = require('multer-storage-cloudinary');
+const cloudinary = require('cloudinary');
+const createCloudinaryStorage = require('multer-storage-cloudinary');
+
+const cloudinaryV2 = cloudinary.v2;
 
 // Configure Cloudinary
-cloudinary.config({
+cloudinaryV2.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
   api_secret: process.env.CLOUDINARY_API_SECRET
 });
 
 // Configure Cloudinary storage for Multer
-const storage = new CloudinaryStorage({
-  cloudinary: cloudinary,
+const storage = createCloudinaryStorage({
+  cloudinary,
   folder: 'portfolio',
   allowedFormats: ['jpg', 'jpeg', 'png', 'gif', 'webp', 'mp4', 'mov', 'avi', 'webm'],
   transformation: [{ width: 1200, crop: "limit" }]
 });
 
 module.exports = {
-  cloudinary,
+  cloudinary: cloudinaryV2,
   storage
 };
